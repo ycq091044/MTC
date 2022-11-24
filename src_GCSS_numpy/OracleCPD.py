21076024dd57897a445168f0ff4b40282cd60791
Chaoqi Yang <ycqsjtu@gmail.com>
2022-11-24 16:18:19 -0600

    fix minor

--- src_GCSS_numpy/OracleCPD.py
+++ src_GCSS_numpy/OracleCPD.py
@@ -9,7 +9,7 @@ import time
 def optimize(u, A, B):
 
     # u = np.linalg.solve(A + np.eye(A.shape[1]) * 1e-8, B)
-    L = la.cholesky(A_ + np.eye(A_.shape[1]) * 1e-8)
+    L = la.cholesky(A + np.eye(A.shape[1]) * 1e-8)
     y = la.solve_triangular(L.T, B, lower=True)
     u = la.solve_triangular(L, y, lower=False)
     return u
